5f0197c1a56abb881f4a0a2c9eb5a2357f2c3af7
KasunShyamal <89163354+KasunShyamal@users.noreply.github.com>
2024-10-05 09:00:19 +0530

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Welcome';
+$route['default_controller'] = 'FormGenerationController';
+$route['form/get_structure/(:num)'] = 'FormGenerationController/fetch_structure/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
